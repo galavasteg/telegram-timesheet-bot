1b5543a219c4ef50d87c61e4fb24860729227396
Artem Shakurov <galavasteg@gmail.com>
2020-09-29 02:08:05 +0300

    utils types

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
-from pathlib import Path
+import os
+from typing import Union, Text
 
 
-def try_load_dotenv(directory: Path) -> None:
+def try_load_dotenv(directory: Union[Text, os.PathLike, None]) -> None:
     try:
         from dotenv import load_dotenv
     except ImportError:  # dotenv doesn't installed on PRD
