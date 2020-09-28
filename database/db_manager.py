e1a10e293c1b8ae3187157e2b81ff93a7d21e1a0
Artem Shakurov <galavasteg@gmail.com>
2020-09-29 02:02:08 +0300

    db: use manager instance connection

--- database/db_manager.py
+++ database/db_manager.py
@@ -33,7 +33,7 @@ class DBManager:
 
     @open_connection
     def list_categories(self):
-        categories = self.con.execute('SELECT name FROM default_category')
+        categories = self._cursor.execute('SELECT name FROM default_category')
         result_list = ''
 
         for category in categories:
@@ -43,14 +43,15 @@ class DBManager:
 
     @open_connection
     def try_add_user(self, user, creation_date):
-        existing_user = self.con.execute('SELECT * FROM user WHERE telegram_id = :telegram_id',
+        existing_user = self._cursor.execute('SELECT * FROM user WHERE telegram_id = :telegram_id',
                                          {'telegram_id' : user.id })
-        if existing_user.fetchone() == None:
-            self.con.execute('INSERT INTO user(telegram_id, name, last_name, created_at) VALUES (:telegram_id, :name, :last_name, :created_at)',
+        if existing_user.fetchone() is None:
+            self._cursor.execute('INSERT INTO user(telegram_id, name, last_name, created_at) VALUES (:telegram_id, :name, :last_name, :created_at)',
                              { 'telegram_id' : user.id,
                                'name': user.first_name,
                                'last_name': user.last_name,
                                'created_at': creation_date })
+            self._con.commit()
 
     @open_connection
     def try_start_session(self, user_id, start_date):
@@ -59,18 +60,19 @@ class DBManager:
         if existing_session.fetchone() == None:
 
             default_interval = 20 * 60
-            previous_interval = self.con.execute('SELECT time_interval FROM session WHERE id = (SELECT MAX(id) from session WHERE user_telegram_id = :user_telegram_id)',
+            previous_interval = self._cursor.execute('SELECT time_interval FROM session WHERE id = (SELECT MAX(id) from session WHERE user_telegram_id = :user_telegram_id)',
                                                  {'user_telegram_id': user_id}).fetchone()
             if previous_interval is None:
                 previous_interval = default_interval
             else:
                 previous_interval = previous_interval[0]
 
-            self.con.execute(
+            self._cursor.execute(
                 'INSERT INTO session(user_telegram_id, time_interval, session_start) VALUES (:user_telegram_id, :time_interval, :session_start)',
                 {'user_telegram_id': user_id,
                  'time_interval': previous_interval,
                  'session_start': start_date})
+            self._con.commit()
             return True
         else:
             return False
@@ -80,18 +82,19 @@ class DBManager:
         existing_session = self._get_active_session_id_for_user(user_id)
 
         fetched = existing_session.fetchone()
-        if  fetched is not None:
-            self.con.execute(
+        if fetched is not None:
+            self._cursor.execute(
                 'UPDATE session SET session_stop = :session_stop where id = :id',
                 {'session_stop': stop_date,
                  'id': fetched[0]})
+            self._con.commit()
             return True
         else:
             return False
 
     @open_connection
     def try_stop_event(self, user_id, category, start_date):
-        rowcount = self.con.execute(
+        rowcount = self._cursor.execute(
             """UPDATE timesheet
                SET finish = datetime(strftime('%s', start) + 
                                     (select time_interval from session where session_id = id),  'unixepoch'),
@@ -108,17 +111,19 @@ class DBManager:
             {'user_telegram_id': user_id,
              'start_date': start_date,
              'category': category}).rowcount
+        self._con.commit()
 
         return rowcount == 1;
 
     @open_connection
     def start_event(self, user_id, start_date):
-        res = self.con.execute(
+        res = self._cursor.execute(
             """INSERT INTO timesheet(uuid, session_id, start)
                 VALUES  (:uuid, :session_id, :start_date)""",
             {'session_id': self._get_active_session_id_for_user(user_id).fetchone()[0],
              'start_date': start_date,
              'uuid': str(uuid())})
+        self._con.commit()
 
     @open_connection
     def try_set_step(self, user_id, step):
@@ -128,12 +133,13 @@ class DBManager:
 
         session_id = session_id[0]
 
-        res = self.con.execute(
+        res = self._cursor.execute(
             """UPDATE session
                 set time_interval = :step
                 where id = :session_id""",
             {'session_id': session_id,
              'step': step})
+        self._con.commit()
 
         return True
 
@@ -145,7 +151,7 @@ class DBManager:
 
         session_id = session_id[0]
 
-        step = self.con.execute(
+        step = self._cursor.execute(
             """SELECT time_interval
                 FROM session
                 WHERE id = :session_id""",
@@ -155,7 +161,7 @@ class DBManager:
 
 
     def _get_active_session_id_for_user(self, user_id):
-        return self.con.execute('SELECT id FROM session WHERE user_telegram_id = :user_telegram_id AND session_stop is NULL',
+        return self._cursor.execute('SELECT id FROM session WHERE user_telegram_id = :user_telegram_id AND session_stop is NULL',
                                          {'user_telegram_id': user_id})
 
 
