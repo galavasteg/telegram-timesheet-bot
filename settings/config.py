43d0b2d52174590ffe5354aabede02d25e8c9a96
Artem Shakurov <galavasteg@gmail.com>
2020-12-03 23:22:40 +0300

    fix en defaults

--- settings/config.py
+++ settings/config.py
@@ -19,13 +19,13 @@ PRJ_NAME = 'CheckYourTime'
 env = Env()
 env.read_envfile()
 
-DEBUG_MODE = env.bool('DEBUG_MODE', True)
-LOG_LEVEL = env('LOG_LEVEL', 'INFO').upper()
+DEBUG_MODE = env.bool('DEBUG_MODE', default=True)
+LOG_LEVEL = env('LOG_LEVEL', default='INFO').upper()
 TELEGRAM_API_TOKEN = env.str('TELEGRAM_API_TOKEN')
 
 assert TELEGRAM_API_TOKEN, 'TELEGRAM_API_TOKEN not provided.'
 
-access_ids_file = Path(env.str('ACCESS_IDS_FILE', str(this_dir / 'allowed_accounts.json')))
+access_ids_file = Path(env.str('ACCESS_IDS_FILE', default=str(this_dir / 'allowed_accounts.json')))
 assert access_ids_file.exists(), f'No such file: {access_ids_file=}'
 with access_ids_file.open(encoding='utf-8') as f:
     ACCESS_IDS = set(json.load(f))
